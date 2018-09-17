90e6e768a25466b276c889ff0b834120d8586b0e
xaositectt <xaositectt@gmail.com>
2018-09-17 16:12:20 +0200

    deleted mongoose model from models

--- backend/models/article.js
+++ backend/models/article.js
@@ -1,11 +1,1 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-let articleSchema = new Schema({
-  title: {type: String, required: true, max: 30},
-  content: {type: String, required: true, max: 100},
-  owner: {type: Number, required: true},
-  done: {type: Boolean, required: true}
-})
-
-module.exports = mongoose.model('Article', articleSchema)
+// define article model here
